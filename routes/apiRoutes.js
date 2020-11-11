c1797962a0a63b4cb490ff0d58552e496883679d
Alison Lubar <aslubar@gmail.com>
2020-11-10 21:55:14 -0500

    cleaned up unnecessary comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,22 +1,14 @@
-// * DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique `id` when it's saved. In order to delete a note, you'll need to read all notes from the `db.json` file, remove the note with the given `id` property, and then rewrite the notes to the `db.json` file.
-
-// DEPENDENCY
 var fs = require("fs");
 var path = require("path");
 const { v4: uuidv4 } = require('uuid');
 
-// ROUTING
 module.exports = function (app) {
-
-    // API GET REQUESTS
 
     app.get("/api/notes", function (req, res) {
         fs.readFile(path.join(__dirname, "../db/db.json"), "utf8", function (err, notes) {
             res.json(JSON.parse(notes));
         });
     });
-
-    // API POST REQUEST
 
     app.post("/api/notes", function (req, res) {
         console.log(req.body);
@@ -49,9 +41,5 @@ module.exports = function (app) {
         });
 
     });
-
-
-    // STILL NEED TO DO: Figure out how to delete the test text-- is that something we need to do?
-
 }
 
--- server.js
+++ server.js
@@ -8,11 +8,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
 
-// ROUTER
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-// LISTENER
 app.listen(PORT, function () {
     console.log("App listening on PORT: " + PORT);
 });
